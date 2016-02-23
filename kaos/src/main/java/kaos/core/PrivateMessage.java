eb527b345805fe0dcba3eba97df77ee0d03b18b7
davve94 <fodavid@student.chalmers.se>
2016-02-23 14:24:29 +0100

    new ISA entities for message created: private & team

---
+++ kaos/src/main/java/kaos/core/PrivateMessage.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kaos.core;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author Davidf
+ */
+@Entity
+public class PrivateMessage extends Message {
+
+    private static final long serialVersionUID = 1L;
+
+    public PrivateMessage(Long id, String text, Long time) {
+        super(id, text, time);
+    }
+}
