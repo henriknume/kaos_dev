73740299555e60e793831142cafee01fefe8f621
davve94 <fodavid@student.chalmers.se>
2016-02-23 13:26:20 +0100

    new Message Entity class

---
+++ kaos/src/main/java/kaos/core/Message.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kaos.core;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Davidf
+ */
+@Entity
+public class Message implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Getter
+    @Setter
+    private Long id;
+    @Getter
+    @Setter
+    private String text;
+    @Getter
+    @Setter
+    private Long time;
+    @ManyToOne
+    private User user;
+    
+    public Message(Long id, String text, Long time){
+        this.id = id;
+        this.text = text;
+        this.time = time;
+    }
+    
+    @Override
+    public String toString() {
+        return "kaos.core.Message[ id=" + id + ", text=" + text+",time=" + time +"]";
+    }
+    
+}
