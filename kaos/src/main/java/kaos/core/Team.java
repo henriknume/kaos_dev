a58fd6735e05a21d709b9222981324bff05d598e
davve94 <fodavid@student.chalmers.se>
2016-02-23 12:54:51 +0100

    Entity class Team created with method update name

---
+++ kaos/src/main/java/kaos/core/Team.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kaos.core;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Davidf
+ */
+@Entity
+public class Team implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Getter
+    @Setter
+    @Column(nullable = false)
+    private String name;
+    @Getter
+    @Setter
+    private String password;
+    
+    public Team() {}          // constructor.. needed i think ?
+    
+    public Team(String name){
+        this.name = name;
+    }
+    
+
+    @Override
+    public String toString() {
+        return "kaos.core.Team[ id=" + name + ", pass="+ password +" ]";
+    }
+    
+}
