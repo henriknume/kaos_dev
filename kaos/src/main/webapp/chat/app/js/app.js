dae6a4f001971e01c9bca14704cd9a53c75712ac
DJdavjohns <davjohns@student.chalmers.se>
2016-02-29 11:30:06 +0100

    Refactored to partials

--- kaos/src/main/webapp/chat/app/js/app.js
+++ kaos/src/main/webapp/chat/app/js/app.js
@@ -2,17 +2,32 @@
 
 //Here we define the module of our app. The array contains all other modules 
 //that this one depends on. 
-var chat = angular.module('chat', ['chatControllers'/*, 'chatService'*/]);
+var chat = angular.module('chat', [
+    'chatControllers',
+    'ngRoute'
+    /*, 'chatService'*/]);
 
-/*
- * Don't know if we need this route provider yet, or at all. 
- * 
+
 chat.config(['$routeProvider',
     function ($routeProvider) {  // Injected object $routeProvider
         $routeProvider.
-                when('/chat/:team', {
-                    templateUrl: 'main.html',
-                    controller: 'mainCtrl'
-                });
+            when('/', {
+                templateUrl: 'chat/app/partials/start.html',
+                controller: 'loginCtrl'
+            })
+            .when('/login', {
+                templateUrl: 'chat/app/partials/login.html',
+                controller: 'loginCtrl'
+            })
+            .when('/register', {
+                templateUrl: 'chat/app/partials/register.html',
+                controller: 'loginCtrl'
+            })
+            .when('/chat/:user', {
+                templateUrl: 'chat/app/partials/main.html',
+                controller: 'mainCtrl',
+                css: 'chat/app/css/mainCss.css'
+            });
+            
     }]);
-*/
+
--- kaos/src/main/webapp/chat/app/js/controllers.js
+++ kaos/src/main/webapp/chat/app/js/controllers.js
@@ -4,29 +4,36 @@
 //modules this one depends on. 
 var chatControllers = angular.module('chatControllers', []);
 
+
 //This is our main controller. Our only controller at the moment. 
-chatControllers.controller('mainCtrl', ['$scope', function($scope){
+chatControllers.controller('mainCtrl', ['$scope', '$routeParams', 
+    function($scope, $routeParams){
         
         /*Here we can declare any variables or function we want to 
           access from the HTML page. */
         
+        $scope.currentUser = $routeParams.user;
+        
         //List of all teams
         $scope.teams = ['kaosdev', 'slackerz', 'chalmerz']; 
         
+        $scope.currentTeam;
+        
         //List of all chat messages and the time of their posting
         $scope.log = [{
-            text: 'Welcome to this awesome chat site!', 
+            text: 'Welcome ' + $scope.currentUser + '!', 
             date: new Date(),
-            avatar: '../app/img/system.png'
+            avatar: 'chat/app/img/system.png'
             }];
         
         //This function is called when the user selects a team chat
         $scope.enterChatRoom = function(team){
-            $scope.log.push({
+            $scope.currentTeam = team;
+            $scope.log = [{
                 text: 'Welcome to the chat chanel for ' + team,
                 date: new Date(),
-                avatar: '../app/img/system.png'             
-            });
+                avatar: 'chat/app/img/system.png'             
+            }];
         };
         
         //This function is called when the user writes a new message in a chat
@@ -34,7 +41,12 @@ chatControllers.controller('mainCtrl', ['$scope', function($scope){
             $scope.log.push({
                 text: text,
                 date: new Date(),
-                avatar: '../app/img/profile-icon.png'
+                avatar: 'chat/app/img/profile-icon.png'
             });
         };
+}]);
+
+
+chatControllers.controller('loginCtrl', ['$scope', function($scope){
+        
 }]);
