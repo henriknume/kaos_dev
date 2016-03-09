228839b4b9be4e0bce9bebc62989e2342f5e3375
DJdavjohns <davjohns@student.chalmers.se>
2016-03-09 15:21:49 +0100

    Changes to team create controller

--- kaos/src/main/webapp/team_management/teamCreateController.js
+++ kaos/src/main/webapp/team_management/teamCreateController.js
@@ -13,14 +13,12 @@ chat.controller('TeamCreateController',
         $scope.run = function(){
             $scope.dataLoading = true;
             TeamService.createTeam({team_name: $scope.team_name, password: $scope.password})
-                .then(function (response) {
-                    if (response.success) {
-                        FlashService.Success('Creation successful', true);
-                        //TODO: Return to main page and enter team chat
-                    } else {
-                        FlashService.Error(response.message);
-                        $scope.dataLoading = false;
-                    }
+                .success(function(){
+                    FlashService.Success('Creation successful', true);
+                    //TODO: Return to main page and enter team chat
+                }).error(function(response){
+                    FlashService.Error("An error occurred: " + response.message);
+                    $scope.dataLoading = false;
                 });
         };
         
